5dec8af8ae72f1a35d71dcc591ee03c004ffd1ed
Vladimir Kostyuk <vodemar22864@gmail.com>
2021-08-28 22:43:17 +0300

    bugfix: popup, adaptivity, semantics, buttons and smth else idk

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
-let submitButton = document.querySelector('.popup__submit-button');
+let popupContainer = document.querySelector('.popup__container');
 let popup = document.querySelector('.popup');
 let formElement = document.querySelectorAll('.popup__input');
 let nameInput = document.querySelector('.popup__input_value_name');
@@ -8,13 +8,13 @@ let jobInput = document.querySelector('.popup__input_value_job');
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
 
-function popupOpened() {
-  popup.classList.add('popup_opened');
+function openPopup() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
+  popup.classList.add('popup_opened');
 }
 
-function popupClosed() {
+function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
@@ -22,9 +22,9 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popupClosed();
+  closePopup();
 }
 
-editButton.addEventListener('click', popupOpened);
-closeButton.addEventListener('click', popupClosed);
-submitButton.addEventListener('click', formSubmitHandler);
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+popupContainer.addEventListener('submit', formSubmitHandler);
